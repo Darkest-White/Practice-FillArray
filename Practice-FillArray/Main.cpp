abb06630eaf6addbdc54981ec5179d1d13b794e4
Darkest-White <darkest.white.fox@gmail.com>
2023-04-13 23:28:00 +0300

    Add project files.

---
+++ Practice-FillArray/Main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+
+#define SIZE 20
+
+int main()
+{
+	srand(time(0));
+
+	int arr[SIZE]{};
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		bool alreadyThere = false;
+		int a = rand() % SIZE;
+
+		do
+		{
+			for (int j = 0; j < i; j++)
+			{
+				alreadyThere = false;
+
+				if (arr[j] == a)
+				{
+					a = rand() % SIZE;
+					alreadyThere = true;
+					break;
+				}
+			}
+		} while (alreadyThere);
+
+		arr[i] = a;
+	}
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << "Arr[" << i << "] = " << arr[i] << endl;
+	}
+
+	int min = arr[0];
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (arr[i] < min)
+		{
+			min = arr[i];
+		}
+	}
+
+	cout << "\n";
+	cout << "Min = " << min << endl;
+
+	system("pause");
+}
